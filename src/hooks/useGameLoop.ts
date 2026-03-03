10d47a487f5f72d97e937ae19cb672b8356081b9
Sahibro <sandy276129@gmail.com>
2026-03-03 08:36:07 +0530

    Update useGameLoop.ts

--- src/hooks/useGameLoop.ts
+++ src/hooks/useGameLoop.ts
@@ -1,1 +1,17 @@
+import { useEffect, useRef } from 'react';
 
+export const useGameLoop = (callback: () => void, delay: number) => {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+    const id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+};
